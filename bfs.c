a470c8641fd908142715fd035383e2fe19b1bca1
mantertius <31227589+mantertius@users.noreply.github.com>
2021-05-18 22:13:07 -0300

    terminei bfs mas ta dando erro de execução no hux

--- bfs.c
+++ bfs.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define DEBUG if(1)
+#define DEBUG if(0)
 #define MAX_SIZE 10000
 //https://thehuxley.com/problem/805
 
@@ -110,16 +110,16 @@ void printGraph(graph* gr, int vertexNum)
     for (int i = 0; i < vertexNum; i++)
     {
         adj_list* tmp = gr->vertices[i];
-        printf("v[%d]: ",i);
+        printf("vertices[%d]: ",i);
         while (tmp)
         {
-            printf("v(%d),",tmp->item);
+            printf("v(%d), ",tmp->item);
             tmp= tmp->next;
         }
         printf("\n");
     }
 }
-void printQ(Queue* q)
+void printQ(Queue* q, graph* g)
 {
     adj_list* tmp = q->head;
     while (tmp != NULL)
@@ -130,72 +130,100 @@ void printQ(Queue* q)
     printf("\n");
     
 }
+void theWay(graph* g,int distToSource[], int lasts[],int origin, int destination, int pass)
+{
+    int buffer[distToSource[destination]];
+    //printf("[[%d]]\n",distToSource[destination]);
+    int i = 0;
+    while (pass != origin)
+    {
+        buffer[i] = lasts[pass];
+        pass = lasts[pass];
+        i++;
+    }
+    printf("Caminho entre %d e %d: ",origin,destination);
+    for(int i = distToSource[destination]-1 ; i>= 0 ;i--)
+    {
+        printf("%d => ",buffer[i]);
+    }
+    printf("%d\n",destination);
+    
+}
 void bfs(graph* graph,int origin, int destination, int count)
 {
     for (int i = 0; i < MAX_SIZE; i++)
     {
         graph->visited[i] = 0;
     }
-    
+
+    int verticesNum = count;
     adj_list*  tmp = graph->vertices[origin];  
     Queue* queue = initQ(); 
     int dequeued;
-    graph->visited[origin] = 1;
-    //DEBUG printf("\tgraph->visited[%d] = %d\n",origin,graph->visited[origin]);
 
+    graph->visited[origin] = 1;
     enQueue(queue,origin);
     printf("Iniciando busca em largura a partir de %d\n",origin); //para a primeira entrada
-    DEBUG printQ(queue);
-
-    int lasts[MAX_SIZE];
+    int distToSource[MAX_SIZE];
+        distToSource[origin] = 0;
+    int lasts[MAX_SIZE]; //vai guardar qual o pai do index... se 3 é pai de 2, lasts[2] = 3
     int found = 0;
+    count = 0;
     while (!isEmptyQ(queue))
     {
-        //DEBUG printf("Fila não está vazia ainda.\n");
-        dequeued = deQueue(queue);
+        dequeued = deQueue(queue); //dequeued é o numero que vai ser visitado, 
         tmp = graph->vertices[dequeued];
-        while (tmp != NULL && !graph->visited[tmp->item])
+        if(dequeued == destination) found = 1; //aqui vemos os vertices que ele tem adjacentes
+        while (tmp != NULL)
         {   
-            //DEBUG printf("found == [%d], tmp = [%d]\n",found,tmp->item);
-            //if(tmp->next != NULL) DEBUG printf("Visitando [%d], proximo = [%d]\n",tmp->item,tmp->next->item);
+            if(!graph->visited[tmp->item])
+            {
+                graph->visited[tmp->item] = 1;
+                enQueue(queue,tmp->item);
+                lasts[tmp->item] = dequeued;
+                distToSource[tmp->item] = distToSource[dequeued]+1;
+                //DEBUG printQ(queue,graph);
+            }
+            else
+            {
+                tmp = tmp->next;
+                continue;
+            }
             printf("Iniciando busca em largura a partir de %d\n",tmp->item);
             if(found == 0){ lasts[count] = tmp->item; count++;}
             if(tmp->item == destination)
             {
                 found = 1;
-                //count++;
+                //DEBUG printf("encontrado [%d]!", found);
                 //DEBUG printf("ENCONTRADO depois de %d passagens, ultimo foi [%d]\n",count,lasts[count-1]);
                 //break;
             }
-            if(!graph->visited[tmp->item])
-            {
-               // DEBUG printf("\tgraph->visited[%d] = %d\n",tmp->item,graph->visited[tmp->item]);
-                graph->visited[tmp->item] = 1;
-                enQueue(queue,tmp->item);
-                DEBUG printQ(queue);
-            }
             tmp = tmp->next;
-            count++;
+            //count++;
+        }
 
+    }
+    //node_explanation(graph);
+    printf("\n");
+    for (int i = 0; i < verticesNum; i++)
+    {
+        switch (graph->visited[i])
+        {
+        case 0:
+            printf("%d | - | -\n", i);
+            break;
+        default:
+            if(distToSource[i] == 0) {printf("%d | %d | -\n", i,distToSource[i]); break;}
+            printf("%d | %d | %d\n", i,distToSource[i],lasts[i]);
+            break;
         }
     }
-
-    //node_explanation(graph);
-
-    
+    printf("\n");
     if (found == 0) printf("Sem caminho entre %d e %d\n\n",origin,destination);
     else
     {
-        printf("Caminho entre %d e %d: %d => ",origin,destination,origin);
-        for (int i = 0; i < count; i++)
-        {
-            if (i == count-1)
-            {
-                printf("%d\n",lasts[i]);
-                break;
-            }     
-            printf("%d => ",lasts[i]);
-        }  
+        theWay(graph,distToSource,lasts,origin,destination,destination);
+        printf("\n");
     }
     DEBUG printf("FIM DA BUSCA!\n");
 
@@ -214,16 +242,12 @@ int main()
         scanf("%d",&n2);
         addEdge(graph,n1,n2);
     }
-    
-    //printGraph(graph,verticesNum);
     for (int i = 0; i < verticesNum; i++)
     {
         //DEBUG printf("indo para [%d]\n",i);
         bubbleSort(graph->vertices[i]);
     }
-    printGraph(graph,verticesNum);
-    
-    
+    DEBUG printGraph(graph,verticesNum);
     for (int i = 0; i < testNum; i++)
     {
         int n1,n2;
@@ -231,9 +255,9 @@ int main()
         scanf("%d",&n2); //destination
         printf("--------\n\nCaso de Teste #%d - BFS(%d)\n\n",i+1,n1);
         DEBUG printf("Alvo: [%d]\n",n2);
-        bfs(graph,n1,n2,0);
+        bfs(graph,n1,n2,verticesNum);
     }
-    
+    printf("--------\n");
     
     return 0;
 }
